202700a731075d7527231b0603529cd422abc15b
zhongshan Wang <87266393+ChangGeHundredfold@users.noreply.github.com>
2025-05-26 22:14:33 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,6 +1,13 @@
 from mmdet.apis import DetInferencer
 inferencer = DetInferencer(
+    ## for mask-rcnn
     weights='work_dirs/mask-rcnn_r50_fpn_1x_voc/epoch_4.pth',
+    ## for sparse-rcnn
+    # weights='work_dirs/sparse-rcnn_r50_fpn_1x_voc/epoch_20.pth',
     device='cuda:0'
 )
+## for mask-rcnn
 inferencer('demo/in', out_dir='outputs/in', no_save_pred=False)
+
+## for sparse-rcnn
+inferencer('demo/in', out_dir='outputs/in/sparse_cnn', no_save_pred=False)
